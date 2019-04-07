e9fab25036763747cd218946dcfe2004243e6390
Joseph Curto <44935373+josephCurto@users.noreply.github.com>
2019-04-07 14:01:01 -0400

    Adding error message for fields

--- DoughBoysMVC/Models/CustomerOrder.cs
+++ DoughBoysMVC/Models/CustomerOrder.cs
@@ -2,16 +2,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace DoughBoysMVC.Models
 {
     public class CustomerOrder
     {
-        public string PhoneNo { get; set; }
+       [Required(ErrorMessage = "Please enter your first name")]
         public string FirstName { get; set; }
+
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string PhoneNo { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(".+\\@.+\\..+", 
+        ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
+        
         public int OrderNo { get; set; }
         public string PickUpDate { get; set; }
 
